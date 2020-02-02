535b330f4139453df3c7d24347920e96437faac1
liuziangexit <liuziang@liuziangexit.com>
2020-02-02 21:09:14 +0800

    首个简单的demo已经完成，但是是多线程做的并发，接下来我们需要将它改成用uthread实现并发；

--- src/linux/uthread_linux_hook_impl.c
+++ src/linux/uthread_linux_hook_impl.c
@@ -22,6 +22,7 @@
 #endif
 #include "../../include/uthread.h"
 #include "../common/assert_helper.h"
+#include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -41,22 +42,34 @@ static bool uimpl_epoll_init(uthread_executor_t *exec) {
   return true;
 }
 
-static void uimpl_epoll_add(uthread_t *handle, int epoll, int fd, int events) {
+static void uimpl_epoll_add(int epoll, uthread_t *handle, int fd, int events) {
   struct epoll_event ev;
   memset(&ev, 0, sizeof(struct epoll_event));
   ev.events = events;
   ev.data.ptr = handle;
-  UTHREAD_CHECK(!epoll_ctl(epoll, EPOLL_CTL_ADD, fd, &ev), "epoll_ctl failed");
+  UTHREAD_CHECK(epoll_ctl(epoll, EPOLL_CTL_ADD, fd, &ev) != -1,
+                "uimpl_epoll_add epoll_ctl failed");
+}
+
+static void uimpl_epoll_del(int epoll, int fd) {
+  UTHREAD_CHECK(epoll_ctl(epoll, EPOLL_CTL_DEL, fd, 0) != -1,
+                "uimpl_epoll_add epoll_ctl failed");
 }
 
 // switch to a uthread that has a epoll event pending on
 static void uimpl_epoll_switch(int epoll, uthread_t *cur_uthread) {
   cur_uthread->state = WAITING_IO;
   struct epoll_event ev;
-  UTHREAD_CHECK(epoll_wait(epoll, &ev, 1, -1) >= 0, "epoll_wait failed");
-#ifdef UTHREAD_DEBUG
-  // under current implementation we only have EPOLLIN
-  UTHREAD_CHECK((ev.events & EPOLLIN) != 0, "uthread internal bug");
+#ifdef UTHREAD_HOOK_STDOUT
+  printf("[exec: %" PRIxPTR ", ut: %" PRIxPTR "]: begin epoll_wait\n",
+         (uintptr_t)uthread_current(EXECUTOR_CLS),
+         (uintptr_t)uthread_current(UTHREAD_CLS));
+#endif
+  UTHREAD_CHECK(epoll_wait(epoll, &ev, 1, -1) != -1, "epoll_wait failed");
+#ifdef UTHREAD_HOOK_STDOUT
+  printf("[exec: %" PRIxPTR ", ut: %" PRIxPTR "]: end epoll_wait\n",
+         (uintptr_t)uthread_current(EXECUTOR_CLS),
+         (uintptr_t)uthread_current(UTHREAD_CLS));
 #endif
   // if another uthread(the fd bind with it) becomes readable, we switch to that
   // thread.
@@ -65,6 +78,19 @@ static void uimpl_epoll_switch(int epoll, uthread_t *cur_uthread) {
     uthread_switch(ev.data.ptr);
   }
   cur_uthread->state = RUNNING;
+}
+
+// return the previous option
+static bool uimpl_set_nonblock(int fd, bool enable) {
+  int flags = fcntl(fd, F_GETFL, 0);
+  UTHREAD_CHECK(flags != -1, "fcntl failed");
+  bool prev = flags & O_NONBLOCK;
+  if (enable)
+    flags |= O_NONBLOCK;
+  else
+    flags &= ~O_NONBLOCK;
+  UTHREAD_CHECK(fcntl(fd, F_SETFL, flags) != -1, "fcntl failed");
+  return prev;
 }
 
 #ifdef __cplusplus
@@ -76,7 +102,10 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
   // TODO check the fd is actually on LISTEN mode
 #endif
 #ifdef UTHREAD_HOOK_STDOUT
-  printf("accept(%d, %" PRIxPTR ", %" PRIxPTR ")\n", sockfd, (uintptr_t)addr,
+  printf("[exec: %" PRIxPTR ", ut: %" PRIxPTR "]: accept(%d, %" PRIxPTR
+         ", %" PRIxPTR ")\n",
+         (uintptr_t)uthread_current(EXECUTOR_CLS),
+         (uintptr_t)uthread_current(UTHREAD_CLS), sockfd, (uintptr_t)addr,
          (uintptr_t)addrlen);
 #endif
   typedef int (*accept_t)(int, struct sockaddr *, socklen_t *);
@@ -85,9 +114,6 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
   UTHREAD_CHECK(real, "retrieve system function accept with dlsym failed")
 #endif
   uthread_executor_t *cur_exec = uthread_current(EXECUTOR_CLS);
-#ifdef UTHREAD_HOOK_STDOUT
-  printf("cur_exec == %" PRIxPTR "\n", (uintptr_t)cur_exec);
-#endif
   if (!cur_exec) {
     // not calling from uthread
     return real(sockfd, addr, addrlen);
@@ -96,43 +122,88 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
   int unread = 0;
   // TODO linux不支持在listen的sock上用ioctl
   // FIONREAD，搞明白epoll是怎么做到的，然后在这里用那个方法
+  // 补充：看起来用select可以做到，不知道性能如何？实际上，本质问题应该描述为“如何检测linux上的文件描述符是否可读？”
   // UTHREAD_CHECK(ioctl(sockfd, FIONREAD, &unread) != -1, "ioctl failed");
   if (unread > 0) {
     // if there is already an incoming client are waiting
     return real(sockfd, addr, addrlen);
   } else {
-    // otherwise switch to another uthread because there is noting to do for
-    // this uthread except wait
+    /* otherwise switch to another uthread because there is noting to do for
+    this uthread except wait*/
     // 1. create epoll for the current executor if it doesn't exist
     UTHREAD_CHECK(uimpl_epoll_init(cur_exec), "can not create epoll");
     // 2. add listening socket into epoll's interest list
-    uimpl_epoll_add(&cur_exec->threads[cur_exec->current], cur_exec->epoll,
+    uimpl_epoll_add(cur_exec->epoll, &cur_exec->threads[cur_exec->current],
                     sockfd, EPOLLIN);
     // 3. switch to other uthread
     uimpl_epoll_switch(cur_exec->epoll, &cur_exec->threads[cur_exec->current]);
-    // 4. when the control flow goes back to this uthread, do accept
+    // 4. when the control flow goes back, remove the socket from epoll interest
+    // list
+    uimpl_epoll_del(cur_exec->epoll, sockfd);
+    // 5. do accept
     return real(sockfd, addr, addrlen);
   }
 }
 
-/*
 int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 #ifdef UTHREAD_HOOK_STDOUT
-  printf("connect(%d, %" PRIxPTR ", %" PRIxPTR ")\n", sockfd, (uintptr_t)addr,
-         (uintptr_t)addrlen);
+  printf("[exec: %" PRIxPTR ", ut: %" PRIxPTR "]: connect(%d, %" PRIxPTR
+         ", %d)\n",
+         (uintptr_t)uthread_current(EXECUTOR_CLS),
+         (uintptr_t)uthread_current(UTHREAD_CLS), sockfd, (uintptr_t)addr,
+         (int)addrlen);
 #endif
-  uthread_executor_t *cur_exec = uthread_impl_current_exec();
-#ifdef UTHREAD_HOOK_STDOUT
-  printf("cur_exec == %" PRIxPTR "\n", (uintptr_t)cur_exec);
-#endif
-  UTHREAD_CHECK(cur_exec, "connect are not calling under uthread");
-  UTHREAD_CHECK(uthread_impl_init_epoll(cur_exec), "can not initialize epoll");
-
   typedef int (*connect_t)(int, const struct sockaddr *, socklen_t);
   connect_t real = (connect_t)dlsym(RTLD_NEXT, "connect");
-  return real(sockfd, addr, addrlen);
+#ifdef UTHREAD_DEBUG
+  UTHREAD_CHECK(real, "retrieve system function connect with dlsym failed")
+#endif
+  uthread_executor_t *cur_exec = uthread_current(EXECUTOR_CLS);
+  if (!cur_exec) {
+    // not calling from uthread
+    return real(sockfd, addr, addrlen);
+  }
+  // calling from uthread
+  /*
+  based on http://man7.org/linux/man-pages/man2/connect.2.html
+  asynchronous call the connect, and switch to other uthread.
+  once the control flow goes back we check the err code and just return it.
+  */
+  // 1. create epoll for the current executor if it doesn't exist
+  UTHREAD_CHECK(uimpl_epoll_init(cur_exec), "can not create epoll");
+  // 2. add connecting socket into epoll's interest list
+  uimpl_epoll_add(cur_exec->epoll, &cur_exec->threads[cur_exec->current],
+                  sockfd, EPOLLOUT);
+  // 3. set the fd to non-block mode
+  bool nonblock = uimpl_set_nonblock(sockfd, true);
+  // 4. call connect
+  int ret = real(sockfd, addr, addrlen);
+  if (ret != 0 && errno != EAGAIN && errno != EINPROGRESS) {
+    return -1;
+  }
+  // 5. switch to other uthread
+  uimpl_epoll_switch(cur_exec->epoll, &cur_exec->threads[cur_exec->current]);
+  // 6. when the control flow goes back, remove the socket from epoll interest
+  // list
+  uimpl_epoll_del(cur_exec->epoll, sockfd);
+  // 7. check whether the operation completed successfully
+  int err;
+  socklen_t errlen = sizeof(err);
+  UTHREAD_CHECK(getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &err, &errlen) != -1,
+                "getsockopt failed");
+  // 8. recover the previous non-block option
+  if (!nonblock) {
+    uimpl_set_nonblock(sockfd, false);
+  }
+  // 9. return the err code of connect operation
+  if (err == 0) {
+    return 0;
+  } else {
+    errno = err;
+    return -1;
+  }
 }
-
+/*
 ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
 #ifdef UTHREAD_HOOK_STDOUT
   printf("send(%d, %" PRIxPTR ", %zu, %d)\n", sockfd, (uintptr_t)buf, len,
--- test/tcp.c
+++ test/tcp.c
@@ -1,12 +1,23 @@
 #include "../include/uthread.h"
+#include <arpa/inet.h>
+#include <errno.h>
 #include <memory.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+
+// TODO 支持在uthread中创建uthread，支持自动扩容
+
+// thread clientt wait until thread servert begins listen
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+bool listen_has_begun = false;
 
 typedef struct {
   int fd;
@@ -15,7 +26,7 @@ typedef struct {
 
 void server_work_thread(void *arg) {
   client_info *client = (client_info *)arg;
-  printf("server_co handling new client\n");
+  printf("server handling new client\n");
   /*
   char buffer[256];
   bzero(buffer, 256);
@@ -35,26 +46,28 @@ void server_accept_thread(void *arg) {
   struct sockaddr_in remote_addr;
   socklen_t remote_addr_len = sizeof(remote_addr);
   while (1) {
+    printf("server begin accept\n");
     int client_fd =
         accept(server_fd, (struct sockaddr *)&remote_addr, &remote_addr_len);
     if (client_fd < 0) {
-      printf("error: accept failed\n");
-      uthread_exit();
+      printf("server accept failed\n");
+      abort();
     }
+    printf("server end accept\n");
 
-    client_info *c = malloc(sizeof(client_info));
-    c->fd = client_fd;
-    memcpy(&c->addr, &remote_addr, remote_addr_len);
-    uthread_error err;
-    uthread_create(UTHREAD_CLS, &err, uthread_current(EXECUTOR_CLS),
-                   server_work_thread, c);
+    // client_info *c = malloc(sizeof(client_info));
+    // c->fd = client_fd;
+    // memcpy(&c->addr, &remote_addr, remote_addr_len);
+    // uthread_error err;
+    // uthread_create(UTHREAD_CLS, &err, uthread_current(EXECUTOR_CLS),
+    //                server_work_thread, c);
   }
 }
 
 void *server(void *a) {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-    printf("error: socket creation failed\n");
+    printf("server socket creation failed\n");
     pthread_exit(0);
   }
 
@@ -62,16 +75,21 @@ void *server(void *a) {
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(9842);
+  server_addr.sin_port = htons(9844);
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
       0) {
-    printf("error: binding failed\n");
+    printf("server binding failed\n");
     pthread_exit(0);
   }
   if (listen(server_fd, 5)) {
-    printf("error: listen failed\n");
+    printf("server listen failed\n");
     pthread_exit(0);
   }
+  pthread_mutex_lock(&mutex);
+  listen_has_begun = true;
+  printf("server listen_has_begun has been marked as true\n");
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&mutex);
   uthread_error err;
   uthread_executor_t *exec = uthread_create(EXECUTOR_CLS, &err, 5, 0);
   if (err != OK)
@@ -85,16 +103,69 @@ void *server(void *a) {
   pthread_exit(0);
 }
 
+void client_thread(void *a) {
+  int sockfd;
+  struct sockaddr_in their_addr;
+  their_addr.sin_family = AF_INET;
+  their_addr.sin_port = htons(9844);
+  their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    abort();
+  }
+  printf("client begin wait\n");
+  pthread_mutex_lock(&mutex);
+  while (!listen_has_begun) {
+    pthread_cond_wait(&cond, &mutex);
+  }
+  pthread_mutex_unlock(&mutex);
+  printf("client end wait\n");
+
+  printf("client begin connect\n");
+  if (connect(sockfd, (struct sockaddr *)&their_addr,
+              sizeof(struct sockaddr)) == -1) {
+    printf("client abort connect with errno = %d\n", errno);
+    abort();
+  }
+  printf("client end connect\n");
+  close(sockfd);
+  // while (1) {
+  //   if (send(sockfd, "Hello, world!\n", 14, 0) == -1) {
+  //      abort();
+  //   }
+  //   printf("After the send function \n");
+
+  //   if ((numbytes = recv(sockfd, buf, MAXDATASIZE, 0)) == -1) {
+  //     perror("recv");
+  //     exit(1);
+  //   }
+
+  //   buf[numbytes] = '\0';
+
+  //   printf("Received in pid=%d, text=: %s \n", getpid(), buf);
+  //   sleep(1);
+  // }
+  uthread_exit();
+}
+
 void *client(void *a) {
-  printf("c\n");
+  uthread_error err;
+  uthread_executor_t *exec = uthread_create(EXECUTOR_CLS, &err, 5, 0);
+  if (err != OK)
+    abort();
+  uthread_create(UTHREAD_CLS, &err, exec, client_thread, 0);
+  if (err != OK)
+    abort();
+  uthread_join(exec);
+  uthread_destroy(EXECUTOR_CLS, exec, 0);
   pthread_exit(0);
 }
 
 int main(int argc, char **args) {
   pthread_t servert, clientt;
   pthread_create(&servert, 0, server, 0);
-  //pthread_create(&clientt, 0, client, 0);
+  pthread_create(&clientt, 0, client, 0);
 
   pthread_join(servert, 0);
-  //pthread_join(clientt, 0);
+  pthread_join(clientt, 0);
 }
